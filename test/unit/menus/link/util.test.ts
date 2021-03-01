318bfb6ad70117598a0fcfa445c3e7834926b002
haha <390519764@qq.com>
2021-03-02 07:45:21 +0800

    test: add test

--- test/unit/menus/link/util.test.ts
+++ test/unit/menus/link/util.test.ts
@@ -2,6 +2,11 @@ import { insertHtml } from '../../../../src/menus/link/util'
 
 describe('测试insertHtml函数', () => {
     test('选区achoreNode和focusNode是同一个且外层只有p标签包裹', () => {
-        const selection = 
+        document.body.innerHTML = `<p id="link">123456</p>`
+        const p = document.getElementById('link')
+        const anchorNode = p.childNodes[0]
+        const selection = { anchorNode: anchorNode, focusNode: anchorNode, anchorOffset: 2, focusOffset: 5 }
+
+        const htmlString = insertHtml(selection as Selection, '123456')
     })
 })
